7b3ebbf4e7f3af61ad19526d837391cf3bad4a02
rob boll <robertcboll@gmail.com>
2015-09-23 22:10:20 -0400

    updates

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -15,27 +14,34 @@ import (
 
 var groupName string
 var outputFile string
+var protocol string
+var port string
 var usePublicIP bool
 
 func init() {
 	flag.StringVar(&groupName, "group", "", "autoscaling group name")
 	flag.StringVar(&outputFile, "output-file", "/etc/sysconfig/etcd-members", "output file: default /etc/sysconfig/etcd-members")
+	flag.StringVar(&protocol, "protocol", "https://", "protocol to prefix, without :// ip: default https")
+	flag.StringVar(&port, "port", "2379", "etcd port")
 	flag.BoolVar(&usePublicIP, "use-public-ip", false, "use public ip: default false")
 }
 
 func main() {
 	flag.Parse()
 	if groupName == "" {
-		log.Fatal("Group name is required.")
+		println("Group name is required.")
+		os.Exit(1)
 	}
 	if outputFile == "" {
-		log.Fatal("Output file is required.")
+		println("Output file is required.")
+		os.Exit(1)
 	}
 
 	metadata := ec2metadata.New(&ec2metadata.Config{})
 	region, err := metadata.Region()
 	if err != nil {
-		log.Fatal(err)
+		println(err.Error())
+		os.Exit(1)
 	}
 
 	config := &aws.Config{
@@ -47,12 +53,13 @@ func main() {
 		AutoScalingGroupNames: []*string{&groupName},
 	})
 	if err != nil {
-		log.Fatal(err)
+		println(err.Error())
+		os.Exit(1)
 	}
 
 	if len(groups.AutoScalingGroups) != 1 {
-		fmt.Println(groups)
-		log.Fatalf("Expected 1 autoscaling group, found %d.", len(groups.AutoScalingGroups))
+		println("Expected 1 autoscaling group.")
+		os.Exit(1)
 	}
 	for _, group := range groups.AutoScalingGroups {
 		instanceIds := []*string{}
@@ -65,7 +72,8 @@ func main() {
 			InstanceIds: instanceIds,
 		})
 		if err != nil {
-			log.Fatal(err)
+			println(err.Error())
+			os.Exit(1)
 		}
 
 		output := bytes.Buffer{}
@@ -81,18 +89,20 @@ func main() {
 				} else {
 					ip = instance.PrivateIpAddress
 				}
-				output.WriteString(fmt.Sprintf("%s=%s", *instance.InstanceId, *ip))
+				output.WriteString(fmt.Sprintf("%s://%s=%s:%s", protocol, *instance.InstanceId, *ip, port))
 			}
 		}
 
 		file, err := os.Create(outputFile)
 		if err != nil {
-			log.Fatal(err)
+			println(err.Error())
+			os.Exit(1)
 		}
 		output.WriteTo(file)
 		err = file.Close()
 		if err != nil {
-			log.Fatal(err)
+			println(err.Error())
+			os.Exit(1)
 		}
 		os.Exit(0)
 	}
